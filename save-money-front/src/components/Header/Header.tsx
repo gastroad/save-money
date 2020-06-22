bf75864bd4b906a7bca31cf8bca7f7cb2490767b
gostroad <gostroad37@gmail.com>
2020-06-22 09:09:01 +0900

    nav 설정

--- save-money-front/src/components/Header/Header.tsx
+++ save-money-front/src/components/Header/Header.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import cx from "classnames";
 
-const Header = () => {
+import { RouteComponentProps, withRouter } from "react-router";
+
+const Header = (props) => {
   return (
     <header>
       <div id="header"></div>
@@ -9,15 +12,34 @@ const Header = () => {
         <h1>SAVE - MONEY</h1>
         <ul>
           <li>
-            <Link to="/dashbaord" className="on">
+            <Link
+              to="/dashboard"
+              className={cx({
+                on: props.location.pathname.includes("/dashboard"),
+              })}
+            >
               DashBaord
             </Link>
           </li>
           <li>
-            <Link to="/gamelist">GameList</Link>
+            <Link
+              to="/gamelist"
+              className={cx({
+                on: props.location.pathname.includes("/gamelist"),
+              })}
+            >
+              GameList
+            </Link>
           </li>
           <li>
-            <Link to="/setting">Setting</Link>
+            <Link
+              to="/setting"
+              className={cx({
+                on: props.location.pathname.includes("/setting"),
+              })}
+            >
+              Setting
+            </Link>
           </li>
         </ul>
       </nav>
@@ -25,4 +47,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default withRouter(Header);
